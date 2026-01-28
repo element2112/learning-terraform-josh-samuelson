7977b129ce880eef557af57a1a977e1f9bf7fd61
Brittanie Staton <brittanie.staton@hudl.com>
2026-01-28 11:32:16 -0500

    use free ami lul

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+# data "aws_ami" "app_ami" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"] # uses wildcard to get the latest version
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["979382823631"] # Bitnami
-}
+#   owners = ["979382823631"] # Bitnami
+# }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  ami           = "ami-03ea746da1a2e36e7"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
